0adcb231ae7aa41fc392adaba77a3761eb1a5fb8
vamsikrishna <kantamneni.krishna@wipro.com>
2020-04-08 17:08:54 +0530

    Implemented POST example along with marshalling and un-marshalling by using JAXB

---
+++ Webservices/JAXRSAnnotations/src/com/pnc/webservices/annotations/Dictionary.java
@@ -0,0 +1,34 @@
+package com.pnc.webservices.annotations;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+import org.codehaus.jackson.annotate.JsonPropertyOrder;
+
+@XmlRootElement
+@XmlType(propOrder={"bookName", "bookDesc"})
+@JsonPropertyOrder({"bookName", "bookDesc"})
+public class Dictionary {
+	
+	private String bookName;
+	private String bookDesc;
+	
+	public Dictionary(){
+	}
+
+	public String getBookName() {
+		return bookName;
+	}
+
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+
+	public String getBookDesc() {
+		return bookDesc;
+	}
+
+	public void setBookDesc(String bookDesc) {
+		this.bookDesc = bookDesc;
+	}
+}
---
+++ Webservices/JAXRSAnnotations/src/com/pnc/webservices/annotations/DictionaryController.java
@@ -0,0 +1,19 @@
+package com.pnc.webservices.annotations;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path(value="/dictionary")
+public class DictionaryController {
+	
+	@GET
+	@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON, MediaType.TEXT_XML})
+	public Dictionary getDetails(){
+		Dictionary dictionary = new Dictionary();
+		dictionary.setBookName("Java");
+		dictionary.setBookDesc("Introduced by JamesGosling...");
+		return dictionary;
+	}
+}
